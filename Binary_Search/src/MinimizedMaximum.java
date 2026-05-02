66642ad416e6384dacd0b3dc03a4fb1b2ae7c114
Yusuf-9198 <mohdyusufbeig786@gmail.com>
2026-05-02 14:58:32 +0530

    02 may

---
+++ Binary_Search/src/MinimizedMaximum.java
@@ -0,0 +1,10 @@
+public class MinimizedMaximum {
+    public int minimizedMaximum(int n, int[] arr){
+         
+        return -1;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
